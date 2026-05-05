0192f2724f2d7ac6036f3b904db39d6ef35461a4
Lavanyabn1997 <varshalavanya24@gmail.com>
2026-05-05 15:26:02 +0530

    Update main.tf

--- IAM/main.tf
+++ IAM/main.tf
@@ -2,50 +2,50 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-########################################
+############################################
 # S3 BUCKET
-########################################
+############################################
 resource "aws_s3_bucket" "app_bucket" {
-  bucket = "app-demo-bucket-123456789"
+  bucket = "iam-complete-demo-bucket-987654321"
 }
 
-########################################
-# IAM USER
-########################################
-resource "aws_iam_user" "app_user" {
-  name = "app-user"
+############################################
+# IAM USER (Human Access)
+############################################
+resource "aws_iam_user" "dev_user" {
+  name = "dev-user"
 }
 
-resource "aws_iam_user_login_profile" "app_user_login" {
-  user                    = aws_iam_user.app_user.name
+resource "aws_iam_user_login_profile" "dev_user_login" {
+  user                    = aws_iam_user.dev_user.name
   password_reset_required = true
 }
 
-resource "aws_iam_access_key" "app_user_key" {
-  user = aws_iam_user.app_user.name
+resource "aws_iam_access_key" "dev_user_key" {
+  user = aws_iam_user.dev_user.name
 }
 
-########################################
+############################################
 # IAM GROUP
-########################################
+############################################
 resource "aws_iam_group" "dev_group" {
   name = "dev-group"
 }
 
 resource "aws_iam_user_group_membership" "user_group" {
-  user = aws_iam_user.app_user.name
+  user = aws_iam_user.dev_user.name
 
   groups = [
     aws_iam_group.dev_group.name
   ]
 }
 
-########################################
-# IAM POLICY (User/Group permissions)
-########################################
-resource "aws_iam_policy" "dev_policy" {
-  name        = "DevPolicy"
-  description = "User access policy (S3 read + EC2 describe)"
+############################################
+# IAM POLICY (User/Group Permissions)
+############################################
+resource "aws_iam_policy" "user_policy" {
+  name        = "UserGeneralPolicy"
+  description = "Allow basic AWS read access"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -53,8 +53,8 @@ resource "aws_iam_policy" "dev_policy" {
       {
         Effect = "Allow"
         Action = [
-          "s3:ListAllMyBuckets",
-          "ec2:Describe*"
+          "ec2:Describe*",
+          "s3:ListAllMyBuckets"
         ]
         Resource = "*"
       }
@@ -64,12 +64,12 @@ resource "aws_iam_policy" "dev_policy" {
 
 resource "aws_iam_group_policy_attachment" "group_attach" {
   group      = aws_iam_group.dev_group.name
-  policy_arn = aws_iam_policy.dev_policy.arn
+  policy_arn = aws_iam_policy.user_policy.arn
 }
 
-########################################
-# IAM ROLE FOR EC2 (S3 ACCESS ROLE)
-########################################
+############################################
+# IAM ROLE (for EC2)
+############################################
 resource "aws_iam_role" "ec2_role" {
   name = "ec2-s3-role"
 
@@ -87,9 +87,9 @@ resource "aws_iam_role" "ec2_role" {
   })
 }
 
-########################################
-# IAM POLICY FOR EC2 ROLE (S3 FULL ACCESS)
-########################################
+############################################
+# IAM POLICY FOR EC2 ROLE (S3 ACCESS)
+############################################
 resource "aws_iam_policy" "ec2_s3_policy" {
   name = "EC2S3AccessPolicy"
 
@@ -112,54 +112,54 @@ resource "aws_iam_policy" "ec2_s3_policy" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ec2_role_attach" {
+resource "aws_iam_role_policy_attachment" "role_attach" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = aws_iam_policy.ec2_s3_policy.arn
 }
 
-########################################
-# INSTANCE PROFILE (Required for EC2 Role)
-########################################
+############################################
+# INSTANCE PROFILE (CONNECT ROLE TO EC2)
+############################################
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "ec2-instance-profile"
   role = aws_iam_role.ec2_role.name
 }
 
-########################################
+############################################
 # EC2 INSTANCE
-########################################
+############################################
 resource "aws_instance" "app_server" {
-  ami           = "ami-0c2af51e265bd5e0e" # Amazon Linux (region-specific)
+  ami           = "ami-0c2af51e265bd5e0e"
   instance_type = "t2.micro"
 
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
   tags = {
-    Name = "AppServer"
+    Name = "IAM-Complete-Server"
   }
 }
 
-########################################
+############################################
 # OUTPUTS
-########################################
-output "access_key_id" {
-  value = aws_iam_access_key.app_user_key.id
+############################################
+output "user_access_key" {
+  value = aws_iam_access_key.dev_user_key.id
 }
 
-output "secret_access_key" {
-  value     = aws_iam_access_key.app_user_key.secret
+output "user_secret_key" {
+  value     = aws_iam_access_key.dev_user_key.secret
   sensitive = true
 }
 
 output "console_password" {
-  value     = aws_iam_user_login_profile.app_user_login.password
+  value     = aws_iam_user_login_profile.dev_user_login.password
   sensitive = true
 }
 
-output "s3_bucket_name" {
+output "s3_bucket" {
   value = aws_s3_bucket.app_bucket.bucket
 }
 
-output "ec2_instance_id" {
+output "ec2_id" {
   value = aws_instance.app_server.id
 }
